77ba2b0a983417bbccda725279e7305753497caf
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2019-01-09 13:01:00 +0000

    Sage 2 completed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxiliary/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys( props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functional component, only a class to demo the nested updates from the Modal component
+    componentWillUpdate(){
+        console.log("[OrderSummary.js] Will Update");
+    }
+
+    render() {
+
+        const ingredientSummary = Object.keys( this.props.ingredients)
         .map( igKey => {
-            if ( props.ingredients[ igKey ] > 0 ) {
+            if ( this.props.ingredients[ igKey ] > 0 ) {
                 return ( <li key={ igKey }>
-                            <span style={ {textTransform: 'capitalize'} }>{igKey}</span>: { props.ingredients[ igKey ] }
+                            <span style={ {textTransform: 'capitalize'} }>{igKey}</span>: { this.props.ingredients[ igKey ] }
                         </li>);
             }
             return null;
         });
-    
-    return(
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: £ { props.price.toFixed(2) }</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={ props.purchasedCancelled } >CANCEL</Button>
-            <Button btnType="Success" clicked={ props.purchaseContinue } >OK</Button>
-        </Aux>
 
-    );
+        return(
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: £ { this.props.price.toFixed(2) }</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={ this.props.purchasedCancelled } >CANCEL</Button>
+                <Button btnType="Success" clicked={ this.props.purchaseContinue } >OK</Button>
+            </Aux>
+        );
 
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
